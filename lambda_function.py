c1fd173a032dac67e7442251a6f1e6b698c1fd54
chuanyao17 <chuanyao17@gmail.com>
2023-08-04 13:54:50 -0400

    using headless mode for docker image

--- lambda_function.py
+++ lambda_function.py
@@ -199,7 +199,7 @@ def lambda_handler(event, context):
     print("finished reading files")
     
     chrome_options = ChromeOptions()
-    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
